1ec1315bc37518da6f5bdc164b046f817d09732a
claudiogardusi <claudio.gardusi@hotmail.com>
2025-08-04 16:32:54 -0300

    Developed in class 6

---
+++ aula6/js/script.js
@@ -0,0 +1,11 @@
+// Estrutura de dado
+
+var numero = 10;
+let numro2 = 20;
+const numero3 = 30;
+
+// tipos de dados
+// real (float) -1.2  3.0 
+// inteiro (int) 1 2 3 4 5
+// string "texto" 'texto' `texto`
+// boolean true false 
