12164b71eec94cd07562fadca3cb5991c9224c5a
claudiogardusi <claudio.gardusi@hotmail.com>
2025-08-08 06:25:47 -0300

    Uploading files from lesson 7

---
+++ aula7/assets/js/main.js
@@ -0,0 +1,30 @@
+idade=18
+
+console.log(idade);
+// prompt()
+let algo = prompt("Digite algo");
+
+if (algo == 20) {
+  console.log("O numero é ", algo);
+} else {
+  console.log("è o numero", algo);
+}
+
+// if ternario
+idade = 18;
+
+// let  teste  = idade>=18? 'maior':'menor'
+
+// console.log(teste)
+
+// switch(idade){
+
+// case 18:
+//     console.log('maior de idade')
+//     break
+// case 17:
+//     console.log('menor de idade')
+//     break
+// default:
+//     console.log('Indefinido')
+// }
